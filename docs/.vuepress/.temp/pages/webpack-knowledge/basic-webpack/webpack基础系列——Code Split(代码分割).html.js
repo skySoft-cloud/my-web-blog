cc5b013aff05417cff84f517c0e1980774d336a6
cloud.chen <chenyun@deltaphone.com.cn>
2022-01-18 18:10:14 +0800

    [build:初次提交架构代码]

---
+++ docs/.vuepress/.temp/pages/webpack-knowledge/basic-webpack/webpack基础系列——Code Split(代码分割).html.js
@@ -0,0 +1,27 @@
+export const data = {
+  "key": "v-419a22b9",
+  "path": "/webpack-knowledge/basic-webpack/webpack%E5%9F%BA%E7%A1%80%E7%B3%BB%E5%88%97%E2%80%94%E2%80%94Code%20Split(%E4%BB%A3%E7%A0%81%E5%88%86%E5%89%B2).html",
+  "title": "",
+  "lang": "zh-CN",
+  "frontmatter": {},
+  "excerpt": "",
+  "headers": [
+    {
+      "level": 2,
+      "title": "webpack系列——Code Split(代码分割)",
+      "slug": "webpack系列——code-split-代码分割",
+      "children": []
+    }
+  ],
+  "git": {
+    "updatedTime": 1642496481000,
+    "contributors": [
+      {
+        "name": "cloud.chen",
+        "email": "chenyun@deltaphone.com.cn",
+        "commits": 1
+      }
+    ]
+  },
+  "filePathRelative": "webpack-knowledge/basic-webpack/webpack基础系列——Code Split(代码分割).md"
+}
